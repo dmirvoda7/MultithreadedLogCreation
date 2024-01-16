c0f4a3c12990ac3e3c296100d70ad8883ed1ce9e
denmi <denmi@DESKTOP-6SNINV6>
2024-01-15 19:29:02 -0500

    check in changes

--- MultithreadedLogCreation/Program.cs
+++ MultithreadedLogCreation/Program.cs
@@ -1,44 +1,48 @@
-﻿
+﻿using System.Configuration;
 
 namespace MultithreadedLogCreation;
 
-
 internal class Program
 {
-    const int NThreadCount = 11;
-    const string Path = @"c:\junk\log";
-    const string FileName = "out.txt";
-    const int NTimesToExecute = 9;
-    
+    private static string? _path;
+    private static string? _fileName;
+    static int _nTimesToExecute = 10;
+    static int _nThreadCount = 11;
+
     static void Main(string[] args)
     {
-        var qualifiedFileName = $"{Path}\\{FileName}";
         try
-        {            
-            StartThreads(qualifiedFileName);           
+        {
+            _path = ConfigurationManager.AppSettings["path"]!;
+            _fileName = ConfigurationManager.AppSettings["fileName"]!;
+            _nTimesToExecute = int.Parse(ConfigurationManager.AppSettings["timesToExecute"]!);
+            _nThreadCount = int.Parse(ConfigurationManager.AppSettings["threadCount"]!);
+
+            var qualifiedFileName = $"{_path}\\{_fileName}";
+            StartThreads(qualifiedFileName);
         }
         catch (Exception ex)
         {
             Console.WriteLine(ex.Message);
-        }  
-       
-        Console.WriteLine("All threads complete");        
+        }
+
+        Console.WriteLine("All threads complete");
     }
 
     public static void StartThreads(string fullyQualifiedFileName)
     {
-        CheckExistanceOfDirectoryAndFile(Path, fullyQualifiedFileName);
+        CheckExistenceOfDirectoryAndFile(_path, fullyQualifiedFileName);
 
-        Thread[] workerThreads = new Thread[NThreadCount];            
+        var workerThreads = new Thread[_nThreadCount];
 
-        for (var k = 0; k < NThreadCount; ++k)
-        {                           
+        for (var k = 0; k < _nThreadCount; ++k)
+        {
             workerThreads[k] = new Thread(() => ProcessWrite(fullyQualifiedFileName));
-            workerThreads[k].Start();                
+            workerThreads[k].Start();
         }
 
         /* Wait for all Threads to finish */
-        for (var k = 0; k < NThreadCount; ++k)
+        for (var k = 0; k < _nThreadCount; ++k)
         {
             workerThreads[k].Join();
         }
@@ -46,14 +50,14 @@ internal class Program
 
     private static void ProcessWrite(string fileName)
     {
-        for (var k = 0; k < NTimesToExecute; ++k)
-        {           
+        for (var k = 0; k < _nTimesToExecute; ++k)
+        {
             WriteToFile(fileName, Environment.CurrentManagedThreadId);
-        }       
+        }
     }
 
     // Check existence of directory and file, if not found create it.
-    public static void CheckExistanceOfDirectoryAndFile(string path, string fileName)
+    public static void CheckExistenceOfDirectoryAndFile(string? path, string fileName)
     {
         if (!Directory.Exists(path))
         {
@@ -63,24 +67,26 @@ internal class Program
         if (File.Exists(fileName))
         {
             DeleteFileIfFound(fileName);
-        }       
-        
-        File.Create(fileName).Close();        
-    }  
-    
+        }
+
+        File.Create(fileName).Close();
+    }
+
     public static void DeleteFileIfFound(string fileName)
     {
         File.Delete(fileName);
     }
 
-    private static void WriteToFile( string filePathAndName, int threadId)
+    private static void WriteToFile(string filePathAndName, int threadId)
     {
         using var mutex = new Mutex(false, filePathAndName.Replace("\\", ""));
         var hasHandle = false;
         try
         {
-            hasHandle = mutex.WaitOne(Timeout.Infinite, false);                
+            hasHandle = mutex.WaitOne(Timeout.Infinite, false);
             var fileContents = $"{GlobalCount.Count} {threadId} {DateTime.Now:HH:mm:ss.fff}";
+            if (GlobalCount.Count > 100)
+                return;
             File.AppendAllText(filePathAndName, fileContents + Environment.NewLine);
             GlobalCount.Count += 1;
         }
@@ -89,5 +95,5 @@ internal class Program
             if (hasHandle)
                 mutex.ReleaseMutex();
         }
-    }    
+    }
 }
